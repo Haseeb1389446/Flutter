f0889bb7494f3c6fe9711e25d84af358bb3c6499
Abdul Haseeb <haseeb1389446@gmail.com>
2025-07-20 15:23:21 +0500

    flutter practice

--- multi_child_widget/lib/main.dart
+++ multi_child_widget/lib/main.dart
@@ -129,35 +129,134 @@ void main() {
         //     ],
         //   ),
 
-        body: ListView(
+        // ListView
+
+        // body: ListView(
+        // scrollDirection: Axis.horizontal,
+        //   children: [
+        //     Container(
+        //         alignment: Alignment.center,
+        //         height: 300,
+        //         width: 300,
+        //         color: Colors.yellowAccent,
+        //         child: Text("Box 1"),
+        //     ),
+        //     Container(
+        //         alignment: Alignment.center,
+        //         height: 300,
+        //         width: 300,
+        //         color: Colors.lightGreen,
+        //         child: Text("Box 2"),
+        //       ),
+        //     Container(
+        //         alignment: Alignment.center,
+        //         height: 300,
+        //         width: 300,
+        //         color: Colors.lightGreenAccent,
+        //         child: Text("Box 3"),
+        //       ),
+        //     Container(
+        //         alignment: Alignment.center,
+        //         height: 300,
+        //         width: 300,
+        //         color: Colors.greenAccent,
+        //         child: Text("Box 4"),
+        //       ),
+        //   ],
+        // ),
+
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 10
+            ),
           children: [
             Container(
                 alignment: Alignment.center,
                 height: 300,
                 width: 300,
-                color: Colors.yellowAccent,
-                child: Text("Box 3"),
+                child: Image.asset('assets/images/img1.jpg'),
             ),
             Container(
                 alignment: Alignment.center,
                 height: 300,
                 width: 300,
-                color: Colors.lightGreen,
-                child: Text("Box 1"),
+                child: Image.asset('assets/images/img2.jpg'),
               ),
             Container(
                 alignment: Alignment.center,
                 height: 300,
                 width: 300,
-                color: Colors.lightGreenAccent,
-                child: Text("Box 2"),
+                child: Image.asset('assets/images/img3.jpg'),
               ),
             Container(
                 alignment: Alignment.center,
                 height: 300,
                 width: 300,
-                color: Colors.greenAccent,
-                child: Text("Box 4"),
+                child: Image.asset('assets/images/img4.jpg'),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.lime,
+                child: Text("Box 5"),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.limeAccent,
+                child: Text("Box 6"),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.blueGrey,
+                child: Text("Box 7"),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.yellow,
+                child: Text("Box 8"),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1752801375943-f0cb633f3422?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8'), 
+                  fit: BoxFit.cover
+                  )
+                ),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1752859625900-a7fbeee9b8e3?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                  fit: BoxFit.cover
+                  )
+                ),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.blueGrey,
+                child: Text("Box 11"),
+              ),
+            Container(
+                alignment: Alignment.center,
+                height: 300,
+                width: 300,
+                color: Colors.yellow,
+                child: Text("Box 12"),
               ),
           ],
         ),
