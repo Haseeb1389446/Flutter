e063e25dc39c53fca8863c4802110efe76ddfc4f
Abdul Haseeb <haseeb1389446@gmail.com>
2025-07-28 10:28:54 +0500

    flutter practice

--- flutter_auth_ui/lib/main.dart
+++ flutter_auth_ui/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_ui/firebase_options.dart';
 import 'package:flutter_auth_ui/screens/home.dart';
 import 'package:flutter_auth_ui/screens/login.dart';
 import 'package:flutter_auth_ui/screens/signup.dart';
+import 'package:flutter_core_ui/flutter_core_ui.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((res){
+    print("Firebase Connected Successfully");
+  }).catchError((err){
+    print("The Error is $err");
+  });
+
   runApp(MaterialApp(
     initialRoute: "",
     routes: {
