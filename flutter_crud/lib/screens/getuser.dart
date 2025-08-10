e1c12b0c3872f1b238cbdfcd8f2987fc4ba66806
Abdul Haseeb <haseeb1389446@gmail.com>
2025-08-10 17:06:10 +0500

    flutter practice

--- flutter_crud/lib/screens/getuser.dart
+++ flutter_crud/lib/screens/getuser.dart
@@ -1,28 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class GetUser extends StatelessWidget {
-  const GetUser({super.key});
+  GetUser({super.key});
+
+  final FirebaseFirestore database = FirebaseFirestore.instance;
+  Future<QuerySnapshot<Map<String, dynamic>>> getAllUsers() async {
+    return await database.collection("Users").get();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("All User")),
-      body: ListView.builder(
-        itemCount: 6,
-        itemBuilder: (context, index) {
-        return ListTile(
-          title: Text("UserName"),
-          subtitle: Text("UserEmail"),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.pencil)),
-              IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bin_xmark)),
-            ],
-          ),
-        );
-      },),
+      body: FutureBuilder(
+        future: getAllUsers(),
+        builder: (context, asyncData) {
+          return ListView.builder(
+            itemCount: asyncData.data!.size,
+            itemBuilder: (context, index) {
+          var username = asyncData.data!.docs[index]["userName"];
+          var useremail = asyncData.data!.docs[index]["userEmail"];
+            return ListTile(
+              title: Text("$username"),
+              subtitle: Text("$useremail"),
+              trailing: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.pencil)),
+                  IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bin_xmark)),
+                ],
+              ),
+            );
+          },);
+        }
+      ),
     );
   }
 }
